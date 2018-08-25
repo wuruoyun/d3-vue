b23dd4d75298c9e345d7710a673d7f28f3971d68
wuruoyun <wuruoyun@gmail.com>
2018-08-25 14:36:49 +0800

    Add colorFn as prop to CandleStick component.

--- src/components/CandleSticks.vue
+++ src/components/CandleSticks.vue
@@ -1,7 +1,7 @@
 <template>
   <g class="candle-sticks" stroke-linecap="round" stroke="black">
     <g v-for="(d, i) in data" :key="keyFn ? keyFn(d) : i" 
-      :transform="`translate(${scaleX(d.date)},0)`" :stroke="stroke(d)"
+      :transform="`translate(${scaleX(d.date)},0)`" :stroke="colorFn(d)"
       class="candle-stick">
       <line :y1="scaleY(d.low)" :y2="scaleY(d.high)"/>
       <line :y1="scaleY(d.open)" :y2="scaleY(d.close)" :stroke-width="scaleX.bandwidth()"/>
@@ -15,11 +15,14 @@ import CartesianArray from '../mixins/CartesianArray'
 
 export default {
   mixins: [ CartesianArray ],
-  methods: {
-    stroke (d) {
-      return d.open > d.close ? d3.schemeSet1[0]
-        : d.close > d.open ? d3.schemeSet1[2]
-        : d3.schemeSet1[8]
+  props: {
+    colorFn: {
+      type: Function,
+      default: d => {
+        return d.open > d.close ? d3.schemeSet1[0]
+          : d.close > d.open ? d3.schemeSet1[2]
+          : d3.schemeSet1[8]
+      }
     }
   }
 }
